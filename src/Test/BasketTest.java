a636704b36f3b220c5f6a1d8d4e78f3a3c991d2f
DavHal <d.a.halliday@hotmail.com>
2019-01-13 18:17:58 +0000

    Documenting progress of BasketTest.java

--- src/Test/BasketTest.java
+++ src/Test/BasketTest.java
@@ -1,13 +1,10 @@
-package Test;
-
 import com.davhal.Basket;
 import com.davhal.Item;
 import com.davhal.Order;
 import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertTrue;
-import static junit.framework.TestCase.fail;
+import static junit.framework.TestCase.*;
 
 public class BasketTest {
 
@@ -33,11 +30,54 @@ public class BasketTest {
 
         double totalTestValue = 1D;
 
-        Basket testBasketObject = new Basket();
-        testBasketObject.itemList.add(new Item("itemTest", totalTestValue, 'A' ));
+        try {
+            Basket testBasketObject = new Basket();
 
+            testBasketObject.itemList.add(new Item("itemTest", totalTestValue, 'A'));
+            testBasketObject.itemList.add(new Item("itemTest", totalTestValue, 'A'));
 
+            assertEquals((totalTestValue*2), testBasketObject.totalCost());
 
+        } catch (Exception e) {
+            fail("totalCostMethodTest Failed!" + e.getMessage());
+        }
+    }
+
+    @Test
+    public void skusContainedInBasketTest() {
+
+        try {
+            Basket testBasketObject = new Basket();
+            char expectAmountOfItems = 'A';
+
+            testBasketObject.itemList.add(new Item("itemTest", 0.50D, 'A'));
+            testBasketObject.itemList.add(new Item("itemTest", 0.50D, 'A'));
+            testBasketObject.itemList.add(new Item("itemTest", 0.50D, 'A'));
+
+            char returnedArray[] = testBasketObject.skusContainedInBasket();
+
+            assertEquals(expectAmountOfItems, returnedArray[0]);
+            assertEquals(expectAmountOfItems, returnedArray[1]);
+            assertEquals(expectAmountOfItems, returnedArray[2]);
+
+        } catch (Exception e) {
+            fail("skusContainedInBasketTest Failed!" + e.getMessage());
+        }
+    }
+
+    @Test
+    public void getItemPriceTest(){
+        try {
+            Basket testBasketObject = new Basket();
+            double itemPrice = 0.30D;
+            testBasketObject.itemList.add(new Item("itemTest", itemPrice, 'A'));
+            double returnedItemPrice = testBasketObject.getItemPrice('A');
+
+            assertEquals(itemPrice, returnedItemPrice);
+
+        } catch (Exception e) {
+            fail("getItemPriceTest Failed!" + e.getMessage());
+        }
     }
 
 
